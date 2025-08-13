a4b27b84b57eef0a55aab3635ec2f7674ca35daa
DevShankar <pandey.dev.2002@gmail.com>
2025-08-13 13:46:12 +0530

    create post

---
+++ src/main/java/com/dev/blogServer/controler/PostController.java
@@ -0,0 +1,28 @@
+package com.dev.blogServer.controler;
+
+import com.dev.blogServer.entity.Post;
+import com.dev.blogServer.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/posts")
+@CrossOrigin(origins = "*")
+public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    @PostMapping
+    public ResponseEntity<?> createPost(@RequestBody Post post){
+        try {
+            Post createdPost = postService.savePost(post);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdPost);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+
+        }
+    }
+}
